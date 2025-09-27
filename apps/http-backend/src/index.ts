87d6fd5edd7299762e0924b237bbd9b68152ed51
Janhavi Patil <patil.janhavi1207@gmail.com>
2025-09-27 15:23:32 +0530

    feat: add jwt in http and ws backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,28 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config"
+import { middleware } from "./middleware"
 
 const app = express()
+
+app.post("/signup", (req,res) => {
+    res.json({
+        userId: "123"
+    })
+})
+
+app.post("/signin", (req,res) => {
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middleware, (req,res) => {
+    res.json()
+})
 
 app.listen(3001);
